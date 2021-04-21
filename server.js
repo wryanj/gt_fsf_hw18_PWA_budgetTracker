0026d03f7591a768eb220b16dc3ce22974b01044
wryanj <ryanjohnson9685@gmail.com>
2021-04-21 17:12:45 -0400

    updated port variable serverjs to try and resolve heroku issue

--- server.js
+++ server.js
@@ -12,7 +12,7 @@
 
 /* ------------------------------ Specify Port ------------------------------ */
   // Define Port
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   // Define Express Instance
   const app = express();
